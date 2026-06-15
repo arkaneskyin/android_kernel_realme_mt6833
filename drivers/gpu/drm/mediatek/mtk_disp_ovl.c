aac192960946a697bb911ad4ed8d3a61b7164ce8
arkaneskyin <shankerudayan2024@gmail.com>
2026-06-15 20:12:42 +0530

    Revert commenting out oplus_mm_kevent_fb.h in drivers/gpu/drm/mediatek/mtk_disp_ovl.c

--- drivers/gpu/drm/mediatek/mtk_disp_ovl.c
+++ drivers/gpu/drm/mediatek/mtk_disp_ovl.c
@@ -12,7 +12,7 @@
 #include <linux/pm_runtime.h>
 #include <linux/soc/mediatek/mtk-cmdq.h>
 #ifdef OPLUS_BUG_STABILITY
-// #include <soc/oplus/system/oplus_mm_kevent_fb.h>
+#include <soc/oplus/system/oplus_mm_kevent_fb.h>
 #endif
 
 #include "mtk_drm_drv.h"
