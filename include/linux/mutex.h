8dfb5594069b8021ae1e3f48ea0fcdd3924cb6cb
arkaneskyin <shankerudayan2024@gmail.com>
2026-06-15 17:28:03 +0530

    Comment out missing OPlus header in include/linux/mutex.h

--- include/linux/mutex.h
+++ include/linux/mutex.h
@@ -92,7 +92,7 @@ struct mutex_waiter {
 };
 
 #ifdef OPLUS_FEATURE_SCHED_ASSIST
-#include <linux/sched_assist/sched_assist_mutex.h>
+// #include <linux/sched_assist/sched_assist_mutex.h> // OPlus proprietary header missing
 #endif /* OPLUS_FEATURE_SCHED_ASSIST */
 
 #ifdef CONFIG_DEBUG_MUTEXES
