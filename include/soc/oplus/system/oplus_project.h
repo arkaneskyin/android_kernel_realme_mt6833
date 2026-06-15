39026c160bc73d53b605fb9487d80e78d8f7dcf1
arkaneskyin <shankerudayan2024@gmail.com>
2026-06-15 21:27:13 +0530

    Update dummy oplus_project.h with get_eng_version

--- include/soc/oplus/system/oplus_project.h
+++ include/soc/oplus/system/oplus_project.h
@@ -1,4 +1,11 @@
 #ifndef _OPLUS_PROJECT_H_
 #define _OPLUS_PROJECT_H_
 
+#define AGING 1
+#define PREVERSION 2
+#define HIGH_TEMP_AGING 3
+#define FACTORY 4
+
+static inline int get_eng_version(void) { return 0; }
+
 #endif
