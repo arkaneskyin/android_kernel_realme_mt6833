14137562ae611df82f262d9f5c470d5c131207d1
arkaneskyin <shankerudayan2024@gmail.com>
2026-06-15 18:04:31 +0530

    Disable missing OPlus feature OPLUS_FEATURE_SCHED_ASSIST in kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1,3 +1,4 @@
+#undef OPLUS_FEATURE_SCHED_ASSIST /* Disable missing proprietary OPlus feature */
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Completely Fair Scheduling (CFS) Class (SCHED_NORMAL/SCHED_BATCH)
