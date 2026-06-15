5bab70ca3354f7ced63a239e67ce8a3d24547b08
arkaneskyin <shankerudayan2024@gmail.com>
2026-06-15 20:12:45 +0530

    Revert commenting out oplus_mm_kevent_fb.h in drivers/gpu/drm/mediatek/mtk_disp_rdma.c

--- drivers/gpu/drm/mediatek/mtk_disp_rdma.c
+++ drivers/gpu/drm/mediatek/mtk_disp_rdma.c
@@ -30,7 +30,7 @@
 #include "oplus_display_onscreenfingerprint.h"
 #endif
 #ifdef OPLUS_BUG_STABILITY
-// #include <soc/oplus/system/oplus_mm_kevent_fb.h>
+#include <soc/oplus/system/oplus_mm_kevent_fb.h>
 #endif
 
 #define DISP_REG_RDMA_INT_ENABLE 0x0000
