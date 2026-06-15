bae5045fa8c6925743e43796ba798f15c15706a8
arkaneskyin <shankerudayan2024@gmail.com>
2026-06-15 20:13:34 +0530

    Add dummy header for oplus_mm_kevent_fb.h

---
+++ vendor/oplus/kernel/system/include/oplus_mm_kevent_fb.h
@@ -0,0 +1,8 @@
+#ifndef _OPLUS_MM_KEVENT_FB_H_
+#define _OPLUS_MM_KEVENT_FB_H_
+
+#define MM_FB_KEY_RATELIMIT_1H 1
+
+static inline void mm_fb_display_kevent(const char *event, int ratelimit, const char *fmt, ...) {}
+
+#endif
